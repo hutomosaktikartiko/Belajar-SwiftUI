6f6cbb98afcd8f962876e580c980f86c82c85dff
Hutomo Sakti Kartiko <hutomosaktikartiko.siskom@gmail.com>
2021-07-17 22:14:41 +0700

    Furniture App - Home Screen

--- Furniture-Shop-App-UI-SwiftUI/Furniture_app/ContentView.swift
+++ Furniture-Shop-App-UI-SwiftUI/Furniture_app/ContentView.swift
@@ -9,22 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color("Bg").edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            VStack {
-                HStack {
-                    Button(action: {}) {
-                        Image("menu").padding().background(Color(.white)).cornerRadius(10.0)
-                    }
-                    
-                    Spacer()
-                    
-                    Button(action: {}) {
-                        Image(uiImage: #imageLiteral(resourceName: "Profile")).resizable().frame(width: 42, height: 42 ).cornerRadius(10.0)
-                    }
-                }
-                .padding(.horizontal)
-            }
+        VStack {
+            HomeScreen()
         }
         
     }
